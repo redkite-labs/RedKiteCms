b02b2a8d2de2a68d8bd7010eda23be2d6d5003dd
alphalemon <g.diblas@email.it>
2012-04-04 22:07:03 +0200

    removed wrong copyright statement

--- src/RedKiteLabs/RedKiteCms/ElFinderBundle/Composer/ScriptHandler.php
+++ src/RedKiteLabs/RedKiteCms/ElFinderBundle/Composer/ScriptHandler.php
@@ -1,22 +1,10 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace AlphaLemon\ElFinderBundle\Composer;
 
 use Composer\Script\Event;
 use Symfony\Component\Process\Process;
 
-/**
- * @author Jordi Boggiano <j.boggiano@seld.be>
- */
 class ScriptHandler
 {
     public static function installElFinderLibrary($event)
