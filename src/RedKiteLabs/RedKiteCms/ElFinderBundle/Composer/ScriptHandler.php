5b4c7ed44a8937a517411791d879c0fc702f2551
alphalemon <g.diblas@email.it>
2012-04-04 21:53:26 +0200

    start composer script

---
+++ src/RedKiteLabs/RedKiteCms/ElFinderBundle/Composer/ScriptHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AlphaLemon\ElFinderBundle\Composer;
+
+use Symfony\Component\Process\Process;
+
+/**
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+class ScriptHandler
+{
+    public static function installElFinderLibrary($event)
+    {
+        $process = new Process('git submodule init');
+        $process->run();
+        
+        $process = new Process('git submodule update');
+        $process->run();
+    }
+}
