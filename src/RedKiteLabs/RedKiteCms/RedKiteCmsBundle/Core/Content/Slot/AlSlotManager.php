304d6575d1a1452db033dc3e16c2095dba2a53ba
alphalemon <g.diblas@email.it>
2012-04-08 11:42:36 +0200

    fixed AlSlotManager to reflect changes in theme engine

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Content/Slot/AlSlotManager.php
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Content/Slot/AlSlotManager.php
@@ -258,7 +258,7 @@ class AlSlotManager extends AlTemplateBase
 
             if (!$rollBack)
             {
-                if($this->useSlotAttributes) $type = $this->slot->getContentType();
+                if($this->useSlotAttributes) $type = $this->slot->getBlockType();
                 $alBlockManager = AlBlockManagerFactory::createBlock($this->container, $type); 
                 $contentValue = array(
                   "PageId"          => $idPage,
@@ -268,7 +268,13 @@ class AlSlotManager extends AlTemplateBase
                   "ContentPosition" => $position
                 );
                 
-                if($this->useSlotAttributes) $contentValue["HtmlContent"] = $this->slot->getDefaultText();
+                if($this->useSlotAttributes) {
+                    $contentValue["HtmlContent"] = $this->slot->getHtmlContent();
+                    $contentValue["ExternalJavascript"] = $this->slot->getExternalJavascript();
+                    $contentValue["InternalJavascript"] = $this->slot->getInternalJavascript();
+                    $contentValue["ExternalStylesheet"] = $this->slot->getExternalStylesheet();
+                    $contentValue["InternalStylesheet"] = $this->slot->getInternalStylesheet();
+                }
                 $rollBack = !$alBlockManager->save($contentValue);
             }
 
