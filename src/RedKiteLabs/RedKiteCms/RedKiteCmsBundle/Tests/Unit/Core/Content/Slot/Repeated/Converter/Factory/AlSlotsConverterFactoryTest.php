fa5bfec7c8bf0631f5da45a3b515f8f377c4fa14
alphalemon <g.diblas@email.it>
2012-05-23 09:27:32 +0200

    added test for the slot converters factory

---
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Tests/Unit/Core/Content/Slot/Repeated/Converter/Factory/AlSlotsConverterFactoryTest.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ * This file is part of the AlphaLemon CMS Application and it is distributed
+ * under the GPL LICENSE Version 2.0. To use this application you must leave
+ * intact this copyright notice.
+ *
+ * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.alphalemon.com
+ * 
+ * @license    GPL LICENSE Version 2.0
+ * 
+ */
+
+namespace AlphaLemon\AlphaLemonCmsBundle\Tests\Unit\Core\Content\Slot\Repeated\Converter\Factory;
+
+use AlphaLemon\AlphaLemonCmsBundle\Tests\TestCase;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Slot\AlSlotManager;
+use AlphaLemon\AlphaLemonCmsBundle\Model\AlPage;
+use AlphaLemon\AlphaLemonCmsBundle\Model\AlLanguage;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Model\AlBlockQuery;
+use AlphaLemon\AlphaLemonCmsBundle\Tests\tools\AlphaLemonDataPopulator;
+use AlphaLemon\ThemeEngineBundle\Core\TemplateSlots\AlSlot;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Slot\Repeated\Converter\Factory\AlSlotsConverterFactory;
+
+class AlSlotsConverterFactoryTest extends TestCase 
+{    
+    protected function setUp() 
+    {
+        parent::setUp();
+        
+        $this->pageContents = $this->getMockBuilder('AlphaLemon\AlphaLemonCmsBundle\Core\Content\PageContentsContainer\AlPageContentsContainer')
+                           ->disableOriginalConstructor()
+                            ->getMock();
+        
+        $this->languageModel = $this->getMockBuilder('AlphaLemon\AlphaLemonCmsBundle\Core\Model\Propel\AlLanguageModelPropel')
+                                    ->disableOriginalConstructor()
+                                    ->getMock();
+        
+        $this->pageModel = $this->getMockBuilder('AlphaLemon\AlphaLemonCmsBundle\Core\Model\Propel\AlPageModelPropel')
+                                    ->disableOriginalConstructor()
+                                    ->getMock();
+        
+        $this->blockModel = $this->getMockBuilder('AlphaLemon\AlphaLemonCmsBundle\Core\Model\Propel\AlBlockModelPropel')
+                                    ->disableOriginalConstructor()
+                                    ->getMock();
+    }
+    
+    /**
+     * @expectedException \AlphaLemon\AlphaLemonCmsBundle\Core\Exception\Content\Slot\SameRepeatedStatusException
+     */
+    public function testCreateConverterThrowsAnExceptionGivingTheSameRepeatedStatus()
+    {
+        $slot = new AlSlot('test', array('repeated' => 'page'));
+        $slotsConverterFactory = new AlSlotsConverterFactory($slot, $this->pageContents, $this->languageModel, $this->pageModel, $this->blockModel);
+        
+        $slotsConverterFactory->createConverter('page');
+    }
+    
+    /**
+     * @expectedException \AlphaLemon\AlphaLemonCmsBundle\Core\Exception\Content\General\ClassNotFoundException
+     */
+    public function testCreateConverterThrowsAnExceptionWhenTheConvertedClassCannotBeInstantiated()
+    {
+        $slot = new AlSlot('test', array('repeated' => 'page'));
+        $slotsConverterFactory = new AlSlotsConverterFactory($slot, $this->pageContents, $this->languageModel, $this->pageModel, $this->blockModel);
+        
+        $slotsConverterFactory->createConverter('fake');
+    }
+    
+    public function testConverterHasBeenInstantiated()
+    {
+        $this->pageContents->expects($this->any())
+            ->method('getSlotBlocks')
+            ->will($this->returnValue(array()));
+        
+        $slot = new AlSlot('test', array('repeated' => 'page'));
+        $slotsConverterFactory = new AlSlotsConverterFactory($slot, $this->pageContents, $this->languageModel, $this->pageModel, $this->blockModel);
+        
+        $slotsConverterFactory->createConverter('site');
+    }
+}
