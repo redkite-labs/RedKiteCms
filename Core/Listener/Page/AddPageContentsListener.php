9b8d6267020328cdcff0e9ac7ffe189b2b49f9c2
alphalemon <g.diblas@email.it>
2012-05-27 10:46:18 +0200

    fixed bockblock typo [ci skip]

--- Core/Listener/Page/AddPageContentsListener.php
+++ Core/Listener/Page/AddPageContentsListener.php
@@ -6,7 +6,7 @@
  *
  * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
  *
- * For the full copyright and license infblockModelation, please view the LICENSE
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * For extra documentation and help please visit http://www.alphalemon.com
