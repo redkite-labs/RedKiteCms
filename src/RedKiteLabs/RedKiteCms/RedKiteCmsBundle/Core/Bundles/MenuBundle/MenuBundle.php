872aa20b2593996d0ac72f22606cf4c67a76a63b
alphalemon <g.diblas@email.it>
2012-07-02 22:41:28 +0200

    removed getAlphaLemonBundleDescription due to new compiler pass blocks loading

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Bundles/MenuBundle/MenuBundle.php
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Bundles/MenuBundle/MenuBundle.php
@@ -6,8 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MenuBundle extends Bundle
 {
-    public function getAlphaLemonBundleDescription()
-    {
-        return 'Menu Content';
-    }
 }
