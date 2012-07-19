96001a27a7b59193c42e539e757ade3f43e036e1
alphalemon <g.diblas@email.it>
2012-07-19 21:53:06 +0200

    fixed tests dependencies and bootstrap [ci-skip]

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -20,7 +20,7 @@
  * Based on https://github.com/FriendsOfSymfony/FOSUserBundle/blob/master/Tests/bootstrap.php which gets all the credits
  */
 
-if (!@include __DIR__ . '/../vendor/.composer/autoload.php') {
+if (!@include __DIR__ . '/../vendor/autoload.php') {
     die(<<<'EOT'
 You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
@@ -29,7 +29,14 @@ EOT
     );
 }
 
-if (class_exists('PropelQuickBuilder') && class_exists('TypehintableBehavior')) {
+//require_once __DIR__ . '/../vendor/propel/propel1/runtime/lib/Propel.php';
+if (0 === strncasecmp(PHP_SAPI, 'cli', 3)) {
+    set_include_path(__DIR__ . '/../vendor/propel/propel1'.PATH_SEPARATOR.get_include_path());
+    set_include_path(__DIR__ . '/../vendor/phing/phing/classes'.PATH_SEPARATOR.get_include_path());
+}
+
+
+if (class_exists('PropelQuickBuilder') && class_exists('TypehintableBehavior')) {// 
     $class = new \ReflectionClass('TypehintableBehavior');
     $builder = new \PropelQuickBuilder();
     $builder->getConfig()->setBuildProperty('behavior.typehintable.class', $class->getFileName());
@@ -38,11 +45,11 @@ if (class_exists('PropelQuickBuilder') && class_exists('TypehintableBehavior')) 
 
     $builder = new \PropelQuickBuilder();
     $builder->getConfig()->setBuildProperty('behavior.typehintable.class', $class->getFileName());
-    $builder->setSchema(file_get_contents(__DIR__.'/../vendor/AlphaLemon/ThemeEngineBundle/Resources/config/schema.xml'));
+    $builder->setSchema(file_get_contents(__DIR__.'/../vendor/alphalemon/theme-engine-bundle/AlphaLemon/ThemeEngineBundle/Resources/config/schema.xml'));
     $builder->buildClasses();
 
-    $queries = explode(";", file_get_contents(__DIR__ . '/Resources/sql/database.sql'));
-    mysql_connect('localhost', 'root', '');
+    $queries = explode(";", file_get_contents(__DIR__ . '/Functional/Resources/sql/database.sql'));
+    mysql_connect('localhost', 'root', 'passera73');
     mysql_select_db('alphalemon_test');
     foreach($queries as $query)
     {
