7cbd55d90061e4683029ab8d80aa45c4c9c8a908
alphalemon <g.diblas@email.it>
2012-07-24 06:45:14 +0200

    removed Core/Bundles folder

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Bundles/NavigationMenuBundle/NavigationMenuBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AlphaLemon\AlphaLemonCmsBundle\Core\Bundles\NavigationMenuBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class NavigationMenuBundle extends Bundle
-{
-}
