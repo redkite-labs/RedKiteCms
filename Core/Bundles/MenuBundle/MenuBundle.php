d0c6c2069273c7d6638f5d8a46c645cdc1d0fa9e
alphalemon <g.diblas@email.it>
2012-07-24 06:45:14 +0200

    removed Core/Bundles folder

--- Core/Bundles/MenuBundle/MenuBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AlphaLemon\AlphaLemonCmsBundle\Core\Bundles\MenuBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MenuBundle extends Bundle
-{
-}
--- Core/Bundles/TextBundle/TextBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AlphaLemon\AlphaLemonCmsBundle\Core\Bundles\TextBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TextBundle extends Bundle
-{
-}
