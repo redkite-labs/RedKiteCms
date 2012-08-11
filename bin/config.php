634fb1aa2bc2e29c1d2edb670b208b93dfc4648b
alphalemon <g.diblas@email.it>
2012-08-11 07:40:41 +0200

    removed bin folder

--- bin/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $companyName = "Acme";
-    $bundleName = "WebSiteBundle";
-    $dsn = "mysql:host=localhost;dbname=alphalemon";
-    $database = "alphalemon";
-    $user = "root";
-    $password = "";
-    $driver = "mysql";
