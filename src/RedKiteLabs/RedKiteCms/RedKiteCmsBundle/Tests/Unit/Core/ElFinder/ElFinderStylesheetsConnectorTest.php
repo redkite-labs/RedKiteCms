8a17efa95736a150e3a582088dd2a4d4dec85fea
alphalemon <g.diblas@email.it>
2012-08-26 15:08:06 +0200

    fixed docblock

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Tests/Unit/Core/ElFinder/ElFinderStylesheetsConnectorTest.php
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Tests/Unit/Core/ElFinder/ElFinderStylesheetsConnectorTest.php
@@ -29,7 +29,7 @@ class ElFinderStylesheetsConnectorExt extends ElFinderStylesheetsConnector
 }
 
 /**
- * AlParametersValidatorLanguagesManager
+ * ElFinderStylesheetsConnectorTest
  *
  * @author AlphaLemon <webmaster@alphalemon.com>
  */
