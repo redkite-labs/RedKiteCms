4b9fe5a110149e9e3cb8d7aacbe5beb2dad321be
alphalemon <user@server>
2012-09-07 19:48:36 +0200

    fixed to rollback only when save fails

--- Core/Content/Template/AlTemplateManager.php
+++ Core/Content/Template/AlTemplateManager.php
@@ -389,6 +389,8 @@ class AlTemplateManager extends AlTemplateBase
      */
     protected function setUpSlotManagers()
     {
+        if (null === $this->template) return;
+        
         $templateSlots = $this->template->getTemplateSlots();
 
         if (null === $templateSlots) {
