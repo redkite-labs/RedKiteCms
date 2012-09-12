b50ac821b0dec950549eaa6907017b4842166871
alphalemon <user@server>
2012-09-12 19:07:47 +0200

    moved EventListenersRegistrator and updated related class namespace

---
+++ src/RedKiteLabs/ThemeEngineBundle/Core/Rendering/Compiler/EventListenersRegistrator.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * This file is part of the AlphaLemonPageTreeBundle and it is distributed
+ * under the MIT License. In addiction, to use this bundle, you must leave
+ * intact this copyright notice.
+ *
+ * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://alphalemon.com
+ * 
+ * @license    MIT License
+ */
+
+namespace AlphaLemon\ThemeEngineBundle\Core\Rendering\Compiler;
+
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Register the listeners by their tags
+ *
+ * @author AlphaLemon
+ */
+class EventListenersRegistrator
+{
+    public static function registerByTaggedServiceId(ContainerBuilder $container, $tagServiceId)
+    {
+        if (!$container->hasDefinition('event_dispatcher')) {
+            return;
+        }
+
+        $definition = $container->getDefinition('event_dispatcher');
+
+        foreach ($container->findTaggedServiceIds($tagServiceId) as $id => $events) {
+            foreach ($events as $event) {
+                $priority = isset($event['priority']) ? $event['priority'] : 0;
+
+                if (!isset($event['event'])) {
+                    throw new \InvalidArgumentException(sprintf('Service "%s" must define the "event" attribute on "%s" tags.', $id, $tagServiceId));
+                }
+
+                if (!isset($event['method'])) {
+                    $event['method'] = 'on'.preg_replace(array(
+                        '/(?<=\b)[a-z]/ie',
+                        '/[^a-z0-9]/i'
+                    ), array('strtoupper("\\0")', ''), $event['event']);
+                }
+
+                $definition->addMethodCall('addListenerService', array($event['event'], array($id, $event['method']), $priority));
+            }
+        }
+    }
+}
--- src/RedKiteLabs/ThemeEngineBundle/Core/Rendering/Compiler/RegisterRenderingListenersPass.php
+++ src/RedKiteLabs/ThemeEngineBundle/Core/Rendering/Compiler/RegisterRenderingListenersPass.php
@@ -10,16 +10,16 @@
  * file that was distributed with this source code.
  *
  * For extra documentation and help please visit http://www.alphalemon.com
- * 
+ *
  * @license    GPL LICENSE Version 2.0
- * 
+ *
  */
 
 namespace AlphaLemon\ThemeEngineBundle\Core\Rendering\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use AlphaLemon\ThemeEngineBundle\Core\Compiler\EventListenersRegistrator;
+use AlphaLemon\ThemeEngineBundle\Core\Rendering\Compiler\EventListenersRegistrator;
 
 class RegisterRenderingListenersPass implements CompilerPassInterface
 {
