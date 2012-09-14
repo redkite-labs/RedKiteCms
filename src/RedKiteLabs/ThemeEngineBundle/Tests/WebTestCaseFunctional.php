76744e55321a38a5330f57f89748d0fcfff8e98a
alphalemon <user@server>
2012-09-14 07:54:49 +0200

    added functional test for ThemesController

---
+++ src/RedKiteLabs/ThemeEngineBundle/Tests/WebTestCaseFunctional.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * This file is part of the AlphaLemon CMS Application and it is distributed
+ * under the GPL LICENSE Version 2.0. To use this application you must leave
+ * intact this copyright notice.
+ *
+ * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
+ *
+ * For the full copyright and license infpageRepositoryation, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.alphalemon.com
+ *
+ * @license    GPL LICENSE Version 2.0
+ *
+ */
+
+namespace AlphaLemon\ThemeEngineBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * WebTestCaseFunctional
+ *
+ * @author alphalemon <webmaster@alphalemon.com>
+ */
+class WebTestCaseFunctional extends WebTestCase
+{
+    protected $client;
+
+    protected function setUp()
+    {
+        $this->client = static::createClient(array(
+            'environment' => 'alcms_test',
+            'debug'       => true,
+            ));
+
+        $activeThemeManager = $this->client->getContainer()->get('alphalemon_theme_engine.active_theme');
+        $activeThemeManager->writeActiveTheme('BusinessWebsiteThemeBundle');
+    }
+}
