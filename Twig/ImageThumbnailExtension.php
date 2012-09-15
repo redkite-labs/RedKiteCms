f2bbd7bc16f9ccea6a79b799d502a125edf52d12
alphalemon <user@server>
2012-09-15 14:09:38 +0200

    added unit tests for twig extensions

--- Twig/ImageThumbnailExtension.php
+++ Twig/ImageThumbnailExtension.php
@@ -18,7 +18,6 @@
 namespace AlphaLemon\AlphaLemonCmsBundle\Twig;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use AlphaLemon\AlphaLemonCmsBundle\Core\ImageThumbnailer\AlImageThumbnailer;
 
 /**
  * Adds the renderSlot function to Twig engine
@@ -29,20 +28,32 @@ class ImageThumbnailExtension extends \Twig_Extension
 {
     private $container;
 
+    /**
+     * Constructor
+     *
+     * @param ContainerInterface $container
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
+    /**
+     * Generates a thumbnail of the given image
+     *
+     * @param string $image
+     * @param int $targetWidth
+     * @param int $targetHeight
+     * @return string
+     */
     public function thumbnail($image, $targetWidth = 100, $targetHeight = 100)
     {
         $imagePath = $this->container->getParameter('kernel.root_dir') . '/../' . $this->container->getParameter('alpha_lemon_cms.web_folder') . $image;
         if (is_file($imagePath)) {
-            $thumbnailer = new AlImageThumbnailer();
+            $thumbnailer = $this->container->get('alpha_lemon_cms.images_thumbnailer');
             $thumbnailer->create($imagePath, $targetWidth, $targetHeight);
-            $size = getimagesize($thumbnailer->getThumbnailImage());
 
-            return sprintf('<img src="%s" width="%s" height="%s" rel="%s" />', dirname($image) .  '/' . $thumbnailer->getThumbnailFolder() . '/' . basename($thumbnailer->getThumbnailImage()), $size[0], $size[1], $image);
+            return sprintf('<img src="%s" width="%s" height="%s" rel="%s" />', dirname($image) .  '/' . $thumbnailer->getThumbnailFolder() . '/' . $thumbnailer->getThumbnailImageName(), $thumbnailer->getThumbnailWidth(), $thumbnailer->getThumbnailHeight(), $image);
         }
     }
 
