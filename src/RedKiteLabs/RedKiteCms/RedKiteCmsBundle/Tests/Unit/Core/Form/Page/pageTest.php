a46514b98f7a5e6e3b2135cf0a79ddf3b253ea12
alphalemon <user@server>
2012-09-22 06:42:48 +0200

    added unit tests for Form namespace

---
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Tests/Unit/Core/Form/Page/pageTest.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * This file is part of the AlphaLemon CMS Application and it is distributed
+ * under the GPL LICENSE Version 2.0. To use this application you must leave
+ * intact this copyright notice.
+ *
+ * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.alphalemon.com
+ *
+ * @license    GPL LICENSE Version 2.0
+ *
+ */
+
+namespace AlphaLemon\AlphaLemonCmsBundle\Tests\Unit\Core\Form\Language;
+
+use AlphaLemon\AlphaLemonCmsBundle\Tests\TestCase;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Form\Page\Page;
+
+/**
+ * PageTest
+ *
+ * @author alphalemon <webmaster@alphalemon.com>
+ */
+class PageTest extends TestCase
+{
+    private $page = null;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->page = new Page();
+    }
+
+    public function testPageName()
+    {
+        $page = 'index';
+        $this->page->setPageName($page);
+        $this->assertEquals($page, $this->page->getPageName());
+    }
+    
+    public function testTemplate()
+    {
+        $template = 'home';
+        $this->page->setTemplate($template);
+        $this->assertEquals($template, $this->page->getTemplate());
+    }
+
+    public function testIsHome()
+    {
+        $this->page->setIsHome(true);
+        $this->assertTrue($this->page->getIsHome());
+    }
+    
+    public function testIsPublished()
+    {
+        $this->page->setIsPublished(true);
+        $this->assertTrue($this->page->getIsPublished());
+    }
+}
---
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Tests/Unit/Core/Form/Seo/SeoTest.php
@@ -0,0 +1,80 @@
+<?php
+/*
+ * This file is part of the AlphaLemon CMS Application and it is distributed
+ * under the GPL LICENSE Version 2.0. To use this application you must leave
+ * intact this copyright notice.
+ *
+ * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.alphalemon.com
+ *
+ * @license    GPL LICENSE Version 2.0
+ *
+ */
+
+namespace AlphaLemon\AlphaLemonCmsBundle\Tests\Unit\Core\Form\Seo;
+
+use AlphaLemon\AlphaLemonCmsBundle\Tests\TestCase;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Form\Seo\Seo;
+
+/**
+ * SeoTest
+ *
+ * @author alphalemon <webmaster@alphalemon.com>
+ */
+class SeoTest extends TestCase
+{
+    private $seo = null;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->seo = new Seo();
+    }
+
+    public function testIdPage()
+    {
+        $page = 2;
+        $this->seo->setIdPage($page);
+        $this->assertEquals($page, $this->seo->getIdPage());
+    }
+    
+    public function testIdLanguage()
+    {
+        $language = 2;
+        $this->seo->setIdLanguage($language);
+        $this->assertEquals($language, $this->seo->getIdLanguage());
+    }
+    
+    public function testPermalink()
+    {
+        $permalink = 'my-permalink';
+        $this->seo->setPermalink($permalink);
+        $this->assertEquals($permalink, $this->seo->getPermalink());
+    }
+    
+    public function testTitle()
+    {
+        $title = 'my title';
+        $this->seo->setTitle($title);
+        $this->assertEquals($title, $this->seo->getTitle());
+    }
+    
+    public function testDescription()
+    {
+        $description = 'my description';
+        $this->seo->setDescription($description);
+        $this->assertEquals($description, $this->seo->getDescription());
+    }
+    
+    public function testKeywords()
+    {
+        $keywords = 'my,keywords';
+        $this->seo->setKeywords($keywords);
+        $this->assertEquals($keywords, $this->seo->getKeywords());
+    }
+}
