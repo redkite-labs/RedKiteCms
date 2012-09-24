8c59db7d4e0d34b1995ae1cfccb6ee7d36860b6a
alphalemon <user@server>
2012-09-24 20:02:04 +0200

    fixed a bug that does not add the default contents of a template, when the active page from where the new page was added, had a different template that the template of the adding page

--- Tests/Functional/Controller/ThemesControllerTest.php
+++ Tests/Functional/Controller/ThemesControllerTest.php
@@ -46,7 +46,7 @@ class ThemesControllerTest extends WebTestCaseFunctional
                                     'MetaKeywords'      => ''));
         self::populateDb();
     }
-    
+
     public function testActiveteThemeWithoutSpecifingThePageToRedirect()
     {
         $crawler = $this->client->request('GET', 'backend/en/al_activateCmsTheme/BusinessWebsiteThemeBundle');
@@ -55,7 +55,7 @@ class ThemesControllerTest extends WebTestCaseFunctional
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertRegExp("/Redirecting to \/backend/s", $crawler->text());
     }
-    
+
     public function testActiveteThemeSpecifingThePageToRedirect()
     {
         $crawler = $this->client->request('GET', 'backend/en/al_activateCmsTheme/BusinessWebsiteThemeBundle/en/page1');
@@ -64,55 +64,55 @@ class ThemesControllerTest extends WebTestCaseFunctional
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertRegExp("/Redirecting to \/backend\/en\/page1/s", $crawler->text());
     }
-    
+
     public function testThemeFixer()
     {
         $params = array("themeName" => "BusinessWebsiteThemeBundle");
         $crawler = $this->client->request('POST', 'backend/en/al_showThemeFixer', $params);
         $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode()); 
+        $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals(1, $crawler->filter('#al_theme_fixer')->count());
-        $this->assertEquals(1, $crawler->filter('#al_theme_fixer_form')->count());        
+        $this->assertEquals(1, $crawler->filter('#al_theme_fixer_form')->count());
         $this->assertEquals(1, $crawler->filter('#al_template')->count());
         $this->assertEquals(2, $crawler->filter('#al_template option')->count());
         $this->assertEquals(1, $crawler->filter('#al_template_changer')->count());
         $this->assertEquals(1, $crawler->filter('#al_pages_to_fix_list')->count());
         $this->assertEquals(1, $crawler->filter('#row_2')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("index")')->count());    
-        $this->assertEquals(1, $crawler->filter('html:contains("home")')->count());    
+        $this->assertEquals(1, $crawler->filter('html:contains("index")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("home")')->count());
         $this->assertEquals(1, $crawler->filter('#row_3')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("page1")')->count());    
-        $this->assertEquals(1, $crawler->filter('html:contains("fullpage")')->count());      
+        $this->assertEquals(1, $crawler->filter('html:contains("page1")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("fullpage")')->count());
         $this->assertEquals(1, $crawler->filter('#al_activate_theme')->count());
     }
-    
+
     public function testChangeTemplateFailsWhenAnyPagesHasBeenSelected()
     {
         $params = array("themeName" => "BusinessWebsiteThemeBundle", "data" => "al_template=fullpage");
         $crawler = $this->client->request('POST', 'backend/en/al_fixTheme', $params);
         $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode()); 
-        $this->assertEquals(1, $crawler->filter('html:contains("Any page has been selected")')->count());    
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('html:contains("Any page has been selected")')->count());
     }
-    
+
     public function testChangeTemplateFailsWhenThePagesDoesNotExist()
     {
-        $params = array("themeName" => "BusinessWebsiteThemeBundle", "data" => "al_template=fullpage&al_page_to_fix=5");
+        $params = array("themeName" => "BusinessWebsiteThemeBundle", "data" => "al_template=fullpage&al_page_to_fix=999");
         $crawler = $this->client->request('POST', 'backend/en/al_fixTheme', $params);
         $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode()); 
-        $this->assertEquals(1, $crawler->filter('html:contains("An error occourced: The following parameters are required: PageName,TemplateName. The parameters you gave are TemplateName")')->count());    
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('html:contains("An error occourced: The following parameters are required: PageName,TemplateName. The parameters you gave are TemplateName")')->count());
     }
-    
+
     public function testChangeTemplate()
     {
         $pageRepository = new AlPageRepositoryPropel();
         $page = $pageRepository->fromPK(2);
-        $this->assertEquals('home', $page->getTemplateName()); 
+        $this->assertEquals('home', $page->getTemplateName());
         $params = array("themeName" => "BusinessWebsiteThemeBundle", "data" => "al_template=fullpage&al_page_to_fix=2");
         $crawler = $this->client->request('POST', 'backend/en/al_fixTheme', $params);
         $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());  
-        $this->assertEquals('fullpage', $page->getTemplateName()); 
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('fullpage', $page->getTemplateName());
     }
 }
