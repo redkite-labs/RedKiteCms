e0d9f482ebb580937d1d677367094c6ad8e6265c
alphalemon <user@server>
2012-09-25 13:40:21 +0200

    added new JsonBlock derived class with container support

---
+++ Core/Content/Block/JsonBlock/AlBlockManagerJsonBlockContainer.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * This file is part of the BusinessMenuBundle and it is distributed
+ * under the GPL LICENSE Version 2.0. To use this application you must leave
+ * intact this copyright notice.
+ *
+ * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.alphalemon.com
+ *
+ * @license    GPL LICENSE Version 2.0
+ *
+ */
+
+namespace AlphaLemon\AlphaLemonCmsBundle\Core\Content\Block\JsonBlock;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Block\JsonBlock\AlBlockManagerJsonBlock;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Validator\AlParametersValidatorInterface;
+
+/**
+ * AlBlockManagerJsonBlockContainer
+ *
+ * @author alphalemon <webmaster@alphalemon.com>
+ */
+abstract class AlBlockManagerJsonBlockContainer extends AlBlockManagerJsonBlock
+{
+    protected $container;
+
+    public function __construct(ContainerInterface $container, AlParametersValidatorInterface $validator = null)
+    {
+        $this->container = $container;
+        $eventsHandler = $container->get('alpha_lemon_cms.events_handler');
+        $factoryRepository = $container->get('alpha_lemon_cms.factory_repository');
+
+        parent::__construct($eventsHandler, $factoryRepository, $validator);
+    }
+}
