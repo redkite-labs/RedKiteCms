dd4b6797498bb19797737ff31d1d177bf98610ec
alphalemon <user@server>
2012-09-29 10:35:44 +0200

    upgraded alphalemon cms frontcontrollers

--- Resources/environments/frontcontrollers/alcms.php
+++ Resources/environments/frontcontrollers/alcms.php
@@ -1,12 +1,24 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/bootstrap_cache.php.cache';
-//require_once __DIR__.'/../app/AppCache.php';
-
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+// Use APC for autoloading to improve performance
+// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
+/*
+$loader = new ApcClassLoader('sf2', $loader);
+$loader->register(true);
+*/
+
+require_once __DIR__.'/../app/AppKernel.php';
+//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('alcms', false);
 $kernel->loadClassCache();
-$kernel->handle(Request::createFromGlobals())->send();
+//$kernel = new AppCache($kernel);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
