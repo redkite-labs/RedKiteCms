9256a3c5b67dc077a1b7c011b00a7611e40a8d63
alphalemon <user@server>
2012-10-08 13:29:16 +0200

    added unit test for GenerateAppThemeBundleCommand

---
+++ src/RedKiteLabs/ThemeEngineBundle/Tests/Unit/Command/Generate/GenerateAppThemeBundleCommandTest.php
@@ -0,0 +1,90 @@
+<?php
+/*
+ * This file is part of the AlphaLemon CMS Application and it is distributed
+ * under the GPL LICENSE Version 2.0. To use this application you must leave
+ * intact this copyright notice.
+ *
+ * Copyright (c) AlphaLemon <webmaster@alphalemon.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.alphalemon.com
+ *
+ * @license    GPL LICENSE Version 2.0
+ *
+ */
+
+namespace AlphaLemon\ThemeEngineBundle\Tests\Unit\Command;
+
+use Sensio\Bundle\GeneratorBundle\Tests\Command\GenerateCommandTest;
+use Symfony\Component\Console\Tester\CommandTester;
+use org\bovigo\vfs\vfsStream;
+
+/**
+ * GenerateAppThemeBundleCommandTest
+ *
+ * @author AlphaLemon <webmaster@alphalemon.com>
+ */
+class GenerateAppThemeBundleCommandTest extends GenerateCommandTest
+{
+
+    /**
+     * @dataProvider getNonInteractiveCommandData
+     */
+    public function testNonInteractiveCommand($options, $expected)
+    {
+        list($namespace, $bundle, $dir, $format, $structure, $strict) = $expected;
+
+        $commandOptions = array(
+            'strict' => $strict,
+        );
+
+        $generator = $this->getGenerator();
+        $generator
+            ->expects($this->once())
+            ->method('generateExt')
+            ->with($namespace, $bundle, $dir, $format, $structure, $commandOptions)
+        ;
+
+        $tester = new CommandTester($this->getCommand($generator, ''));
+        $tester->execute($options, array('interactive' => false));
+    }
+
+    public function getNonInteractiveCommandData()
+    {
+        $root = vfsStream::setup('root');
+
+        return array(
+            array(array('--dir' => vfsStream::url('root'), '--namespace' => 'Foo/BarBundle'), array('Foo\BarBundle', 'FooBarBundle', vfsStream::url('root/'), 'annotation', false, false)),
+            array(array('--dir' => vfsStream::url('root'), '--namespace' => 'Foo/BarBundle', '--format' => 'yml', '--bundle-name' => 'BarBundle', '--structure' => true), array('Foo\BarBundle', 'BarBundle', vfsStream::url('root/'), 'yml', true, false)),
+            array(array('--dir' => vfsStream::url('root'), '--strict' => true, '--namespace' => 'AlphaLemon/Theme/BarThemeBundle', '--format' => 'yml', '--bundle-name' => 'BarThemeBundle', '--structure' => true), array('AlphaLemon\Theme\BarThemeBundle', 'BarThemeBundle', vfsStream::url('root/'), 'yml', true, true)),
+        );
+    }
+
+    protected function getCommand($generator, $input)
+    {
+        $command = $this
+            ->getMockBuilder('AlphaLemon\ThemeEngineBundle\Command\Generate\GenerateAppThemeBundleCommand')
+            ->setMethods(array('checkAutoloader', 'updateKernel'))
+            ->getMock()
+        ;
+
+        $command->setContainer($this->getContainer());
+        $command->setHelperSet($this->getHelperSet($input));
+        $command->setGenerator($generator);
+
+        return $command;
+    }
+
+    protected function getGenerator()
+    {
+        // get a noop generator
+        return $this
+            ->getMockBuilder('AlphaLemon\ThemeEngineBundle\Core\Generator\AlAppThemeGenerator')
+            ->disableOriginalConstructor()
+            ->setMethods(array('generateExt'))
+            ->getMock()
+        ;
+    }
+}
