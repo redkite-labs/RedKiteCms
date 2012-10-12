811b6223bc71e6b01ceb024c204ca8cb29105c58
alphalemon <user@server>
2012-10-12 06:32:14 +0200

    fixed control condition on IsPublished field

--- Tests/Functional/Controller/PagesControllerTest.php
+++ Tests/Functional/Controller/PagesControllerTest.php
@@ -278,7 +278,7 @@ class PagesControllerTest extends WebTestCaseFunctional
                         'pageName' => "page2 edited",
                         'permalink' => "page-2 edited",);
 
-        $crawler = $this->client->request('POST', 'backend/en/al_savePage', $params);echo $crawler->text();exit;
+        $crawler = $this->client->request('POST', 'backend/en/al_savePage', $params);
         $response = $this->client->getResponse();
         $this->assertEquals(200, $response->getStatusCode());
 
