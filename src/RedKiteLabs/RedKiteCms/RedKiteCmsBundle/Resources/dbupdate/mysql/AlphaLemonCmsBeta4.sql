c7e190a0ff9b17c2259392aea9018954ba02cfce
alphalemon <user@server.com>
2012-10-27 06:19:53 +0200

    added a command to upgrade the database to beta4 release

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Resources/dbupdate/mysql/AlphaLemonCmsBeta4.sql
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Resources/dbupdate/mysql/AlphaLemonCmsBeta4.sql
@@ -1,4 +1,7 @@
-ALTER TABLE `al_block` CHANGE `html_content` `content` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
-ALTER TABLE `al_block` CHANGE `class_name` `type` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Text';
-ALTER TABLE `al_language` CHANGE `language` `language_name` VARCHAR( 5 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
-ALTER TABLE `al_language` CHANGE `main_language` `main_language` INT( 1 ) NOT NULL DEFAULT '0';
+CREATE TABLE IF NOT EXISTS `al_locked_resource` (
+  `resource_name` varchar(32) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`resource_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
