5c1e247a7b5a321e56cf733a1a7bcc2ec2024082
alphalemon <user@server.com>
2012-11-23 13:33:10 +0100

    fixed media library not correctly rendered when using the FileBundle

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Bundle/AlphaLemon/Block/FileBundle/Controller/ElFinderFileController.php
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Bundle/AlphaLemon/Block/FileBundle/Controller/ElFinderFileController.php
@@ -11,4 +11,9 @@ class ElFinderFileController extends Controller
         $connector = $this->container->get('el_finder.file_connector');
         $connector->connect();
     }
+    
+    public function showMediaLibraryAction()
+    {
+        return $this->container->get('templating')->renderResponse('FileBundle:Block:file_media_library.html.twig', array('enable_yui_compressor' => $this->container->getParameter('alpha_lemon_cms.enable_yui_compressor')));
+    }
 }
