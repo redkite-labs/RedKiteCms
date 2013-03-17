25ea52f94828229a8646366727077443d00faaf0
alphalemon <user@server.com>
2013-03-17 18:32:11 +0100

    first commit

---
+++ src/RedKiteCms/Theme/BootbusinessThemeBundle/BootbusinessThemeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AlphaLemon\Theme\BootbusinessThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BootbusinessThemeBundle extends Bundle
+{
+}
---
+++ src/RedKiteCms/Theme/BootbusinessThemeBundle/DependencyInjection/BootbusinessThemeExtension.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of the BusinessWebsiteThemeBundle theme and it is distributed
+ * under the MIT License. In addiction, to use this bundle, you must leave
+ * intact this copyright notice.
+ *
+ * (c) Since 2011 AlphaLemon
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://alphalemon.com
+ *
+ * @license    GPL LICENSE Version 2.0
+ *
+ */
+
+namespace  AlphaLemon\Theme\BootbusinessThemeBundle\DependencyInjection;
+
+use AlphaLemon\ThemeEngineBundle\Core\Rendering\DependencyInjection\BaseExtension;
+
+class BootbusinessThemeExtension extends BaseExtension
+{
+    public function configureTheme()
+    {
+        return
+            array(
+                array(
+                    'path' => __DIR__.'/../Resources/config',
+                    'configFiles' =>
+                    array(
+                        'bootbusiness_theme.xml',
+                    ),
+                    'configuration' =>
+                    array(
+                        array(
+                            'path' => __DIR__.'/../Resources/config/templates',
+                            'configFiles' =>
+                            array(
+                                'home.xml',
+                            ),
+                            'configuration' =>
+                            array(
+                                array(
+                                    'path' => __DIR__.'/../Resources/config/templates/slots',
+                                    'configFiles' =>
+                                    array(
+                                        'home.xml',
+                                        'base.xml',
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            );
+    }
+
+    public function getAlias()
+    {
+        return 'bootbusiness_theme';
+    }
+}
