b31ec768c12eb6609cd48c504d7b98235d384983
alphalemon <user@server.com>
2013-03-17 17:11:09 +0100

    added a new integrated bundle to manage a single image

---
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Bundle/AlphaLemon/Block/ImageBundle/Core/Block/AlBlockManagerImage.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * An AlphaLemonCms Block
+ */
+
+namespace AlphaLemon\Block\ImageBundle\Core\Block;
+
+use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Block\JsonBlock\AlBlockManagerJsonBlockContainer;
+
+/**
+ * Description of AlBlockManagerImage
+ */
+class AlBlockManagerImage extends AlBlockManagerJsonBlockContainer
+{
+    public function getDefaultValue()
+    {
+        $value = 
+            '
+                {
+                    "0" : {
+                        "src": "",
+                        "data_src": "holder.js/260x180",
+                        "title" : "Sample title",
+                        "alt" : "Sample alt"
+                    }
+                }
+            ';
+        
+        return array('Content' => $value);
+    }
+    
+    public function getHtml()
+    {
+        $items = $this->decodeJsonContent($this->alBlock->getContent());
+        
+        return array('RenderView' => array(
+            'view' => 'ImageBundle:Image:image.html.twig',
+            'options' => array(
+                'image' => $items[0],
+            ),
+        ));
+    }
+    
+    protected function replaceHtmlCmsActive()
+    {
+        $items = $this->decodeJsonContent($this->alBlock->getContent());
+        $item = $items[0];
+        
+        $formClass = $this->container->get('image.form');
+        $buttonForm = $this->container->get('form.factory')->create($formClass, $item);
+        
+        return array('RenderView' => array(
+            'view' => 'ImageBundle:Editor:image_editor.html.twig',
+            'options' => array(
+                'image' => $item, 
+                'form' => $buttonForm->createView(),
+            ),
+        ));
+    }
+}
