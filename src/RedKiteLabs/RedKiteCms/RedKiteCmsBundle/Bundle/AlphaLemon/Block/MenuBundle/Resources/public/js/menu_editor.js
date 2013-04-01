c66f03bec557ce9321bb36f1b0d22cdedeee5143
alphalemon <user@server.com>
2013-04-01 10:48:22 +0200

    updated internal Menu App-Block to next release

---
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Bundle/AlphaLemon/Block/MenuBundle/Resources/public/js/menu_editor.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $(document).on("blockEditing", function(event, element){
+        if (element.attr('data-type') != 'Menu') {
+            return;
+        }
+        
+        $(element)
+            .find('.al-menu-list')
+            .inlinelist('start')
+        ;
+    });
+    
+    $(document).on("blockStopEditing", function(event, element){ 
+        if (element.attr('data-type') != 'Menu') {
+            return;
+        }
+                
+        $(element)
+            .find('.al-menu-list')
+            .inlinelist('stop')
+        ;
+        $('.al_block_adder').unbind().blocksMenu('initAdders')
+    });
+});
