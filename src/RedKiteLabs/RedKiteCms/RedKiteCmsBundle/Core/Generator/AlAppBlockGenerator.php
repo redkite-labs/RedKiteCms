a2cf7985688414f7aa16ae623fb57e15a8cce92f
alphalemon <user@server.com>
2013-05-07 06:38:30 +0200

    updated App-Blocks generator

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Generator/AlAppBlockGenerator.php
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Generator/AlAppBlockGenerator.php
@@ -30,7 +30,7 @@ class AlAppBlockGenerator extends AlBaseGenerator
 
         $blockSkeletonDir = __DIR__ . '/../../Resources/skeleton/app-block';
         $extensionAlias = Container::underscore($bundleBasename);
-        $typeLowercase = strtolower($bundleBasename);
+        $typeLowercase = strtolower($bundleBasename);        
         $parameters = array(
             'namespace' => $namespace,
             'namespace_path' => str_replace('\\', '\\\\', $namespace),
@@ -42,9 +42,10 @@ class AlAppBlockGenerator extends AlBaseGenerator
             'extension_alias' => $extensionAlias,
             'description'    => $options["description"],
             'group'    => $options["group"],
-        );
-
+        ); 
+        
         $this->renderFile($blockSkeletonDir, 'Block.php', $dir.'/Core/Block/AlBlockManager'.$bundleBasename.'.php', $parameters);
+        $this->renderFile($blockSkeletonDir, 'FormType.php', $dir.'/Core/Form/'.$bundleBasename.'Type.php', $parameters);
         $this->renderFile($blockSkeletonDir, 'app_block.xml', $dir.'/Resources/config/app_block.xml', $parameters);
         $this->renderFile($blockSkeletonDir, 'config_alcms.yml', $dir.'/Resources/config/config_alcms.yml', $parameters);
         $this->renderFile($blockSkeletonDir, 'config_alcms_dev.yml', $dir.'/Resources/config/config_alcms_dev.yml', $parameters);
@@ -53,6 +54,7 @@ class AlAppBlockGenerator extends AlBaseGenerator
         if (!array_key_exists("no-strict", $options) || $options["no-strict"] == false) {
             $this->renderFile($blockSkeletonDir, 'composer.json', $dir.'/composer.json', $parameters);
         }
-        $this->filesystem->copy($blockSkeletonDir . '/editor.html.twig', $dir.'/Resources/views/Block/' . $typeLowercase . '_editor.html.twig');
+        $this->filesystem->copy($blockSkeletonDir . '/block.html.twig', $dir.'/Resources/views/Content/' . $typeLowercase . '.html.twig');
+        $this->filesystem->copy($blockSkeletonDir . '/form_editor.html.twig', $dir.'/Resources/views/Editor/' . $typeLowercase . '.html.twig');
     }
 }
--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Resources/skeleton/app-block/Block.php
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Resources/skeleton/app-block/Block.php
@@ -5,15 +5,49 @@
 
 namespace {{ namespace }}\Core\Block;
 
-use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Block\AlBlockManager;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Block\JsonBlock\AlBlockManagerJsonBlockContainer;
 
 /**
  * Description of AlBlockManager{{ bundle_basename }}
  */
-class AlBlockManager{{ bundle_basename }} extends AlBlockManager
+class AlBlockManager{{ bundle_basename }} extends AlBlockManagerJsonBlockContainer
 {
     public function getDefaultValue()
     {
-        return array('Content' => '<p>Default content</p>');
+        $value = 
+            '
+                {
+                    "0" : {
+                        "block_text": "Default value"
+                    }
+                }
+            ';
+
+        return array('Content' => $value);
+    }
+
+    protected function renderHtml()
+    {
+        $items = $this->decodeJsonContent($this->alBlock->getContent());
+
+        return array('RenderView' => array(
+            'view' => '{{ bundle }}:Content:{{type_lowercase}}.html.twig',
+            'options' => array('item' => $items[0]),
+        ));
+    }
+
+    public function editorParameters()
+    {
+        $items = $this->decodeJsonContent($this->alBlock->getContent());
+        $item = $items[0];
+
+        $formClass = $this->container->get('{{type_lowercase}}.form');
+        $form = $this->container->get('form.factory')->create($formClass, $item);
+
+        return array(
+            "template" => '{{ bundle }}:Editor:{{type_lowercase}}.html.twig',
+            "title" => "My awesome App-Block",
+            "form" => $form->createView(),
+        );
     }
 }
