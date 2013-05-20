28bb15fad97a420207cb09618d0e9d2ab73c8fb1
alphalemon <user@server.com>
2013-05-20 18:19:25 +0200

    added function test for ThemesController

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Tests/Functional/Controller/ThemesControllerTest.php
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Tests/Functional/Controller/ThemesControllerTest.php
@@ -19,6 +19,7 @@ namespace AlphaLemon\AlphaLemonCmsBundle\Tests\Functional\Controller;
 
 use AlphaLemon\AlphaLemonCmsBundle\Tests\WebTestCaseFunctional;
 use AlphaLemon\AlphaLemonCmsBundle\Core\Repository\Propel\AlPageRepositoryPropel;
+use AlphaLemon\AlphaLemonCmsBundle\Core\Repository\Propel\AlBlockRepositoryPropel;
 
 /**
  * ThemesControllerTest
@@ -59,34 +60,126 @@ class ThemesControllerTest extends WebTestCaseFunctional
         $this->assertCount(1, $crawler->filter('#al_template_changer'));
         $this->assertCount(1, $crawler->filter('#al_close_dialog'));
     }
-/*
-    public function testChangeTemplateFailsWhenAnyPagesHasBeenSelected()
+    
+    public function testToChangeAThemeAllTemplatesMustBeMapped()
     {
-        $params = array("themeName" => "BootbusinessThemeBundle", "data" => "al_template=fullpage");
-        $crawler = $this->client->request('POST', 'backend/en/al_fixTheme', $params);
+        $params = array("themeName" => "BootbusinessThemeBundle", "data" => "al-template=home&al-mapped-template=home&al-template=empty&al-mapped-template=");
+        $crawler = $this->client->request('POST', 'backend/en/al_changeTheme', $params);
         $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(1, $crawler->filter('html:contains("Any page has been selected")')->count());
+        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertCount(1, $crawler->filter('html:contains("To change a theme each template must be mapped with a template from the new theme")'));
+        
     }
-
-    public function testChangeTemplateFailsWhenThePagesDoesNotExist()
+    
+    public function testChangeTheme()
     {
-        $params = array("themeName" => "BootbusinessThemeBundle", "data" => "al_template=fullpage&al_page_to_fix=999");
-        $crawler = $this->client->request('POST', 'backend/en/al_fixTheme', $params);
-        $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals(1, $crawler->filter('html:contains("An error occourced: The following options are required: PageName,TemplateName. The options you gave are TemplateName")')->count());
-    }
-
-    public function testChangeTemplate()
-    {
+        $blockRepository = new AlBlockRepositoryPropel();
+        $this->assertCount(0, $blockRepository->retrieveContents(null, null, null, 2));
+        
         $pageRepository = new AlPageRepositoryPropel();
-        $page = $pageRepository->fromPK(2);
+        $page = $pageRepository->fromPageName('index');
         $this->assertEquals('home', $page->getTemplateName());
-        $params = array("themeName" => "BootbusinessThemeBundle", "data" => "al_template=fullpage&al_page_to_fix=2");
-        $crawler = $this->client->request('POST', 'backend/en/al_fixTheme', $params);
+        
+        $page = $pageRepository->fromPageName('page1');
+        $this->assertEquals('empty', $page->getTemplateName());
+        
+        $params = array("themeName" => "SunshineThemeBundle", "data" => "al-template=home&al-mapped-template=home&al-template=empty&al-mapped-template=internal");
+        $crawler = $this->client->request('POST', 'backend/en/al_changeTheme', $params);
         $response = $this->client->getResponse();
         $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('fullpage', $page->getTemplateName());
-    }*/
+        $this->assertCount(1, $crawler->filter('html:contains("The theme has been changed. Please wait while your site is reloading")'));
+       
+        $crawler = $this->client->request('GET', 'backend');
+           
+        $this->assertNotCount(0, $blockRepository->retrieveContents(null, null, null, 2));
+        
+        $page = $pageRepository->fromPageName('index');
+        $this->assertEquals('home', $page->getTemplateName());
+        
+        $page = $pageRepository->fromPageName('page1');
+        $this->assertEquals('internal', $page->getTemplateName());
+    }
+    
+    public function testChangeSlot()
+    {
+        $blockRepository = new AlBlockRepositoryPropel();
+        $sourceBlocks = $blockRepository->retrieveContents(null, null, 'slider_box', 2);
+        $this->assertCount(1, $sourceBlocks);
+        $targetBlocks = $blockRepository->retrieveContents(null, null, 'logo');
+        $this->assertCount(1, $targetBlocks);
+        
+        $params = array("languageId" => 2, "pageId" => 2, "sourceSlotName" => "slider_box", "targetSlotName" => "logo");
+        $crawler = $this->client->request('POST', 'backend/en/al_changeSlot', $params);
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertRegExp('/Content-Type:  application\/json/s', $response->__toString());
+        
+        $json = json_decode($response->getContent(), true);
+        $this->assertEquals(2, count($json));
+        $this->assertTrue(array_key_exists("key", $json[0]));
+        $this->assertEquals("message", $json[0]["key"]);
+        $this->assertTrue(array_key_exists("value", $json[0]));
+        $this->assertEquals("The slot has been changed", $json[0]["value"]);
+        
+        $this->assertTrue(array_key_exists("key", $json[1]));
+        $this->assertEquals("slots", $json[1]["key"]);
+        $this->assertTrue(array_key_exists("value", $json[1]));
+        
+        $changedSourceBlocks = $blockRepository->retrieveContents(null, null, 'slider_box', 3);
+        $this->assertCount(1, $changedSourceBlocks);
+        $changedTargetBlocks = $blockRepository->retrieveContents(null, null, 'logo');
+        $this->assertCount(1, $changedTargetBlocks);
+        
+        $this->assertEquals($changedTargetBlocks[0]->getContent(), $sourceBlocks[0]->getContent());
+        $this->assertEquals($changedSourceBlocks[0]->getContent(), $targetBlocks[0]->getContent());
+    }
+    
+    public function testShowThemesFinalizerChanger()
+    {
+        $crawler = $this->client->request('GET', 'backend/en/al_showThemesFinalizer');
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertCount(1, $crawler->filter('#al-partial-finalizer'));
+        $this->assertCount(1, $crawler->filter('#al-full-finalizer'));
+        $this->assertCount(1, $crawler->filter('#al-close-finalizer'));
+        $this->assertCount(1, $crawler->filter('html:contains("Finalizes the change of theme")'));
+    }
+    
+    public function testPartialFinalization()
+    {
+        $blockRepository = new AlBlockRepositoryPropel();
+        $this->assertCount(1, $blockRepository->retrieveContents(null, null, null, 3));
+        
+        $params = array('action' => 'partial');
+        $crawler = $this->client->request('POST', 'backend/en/al_finalizeTheme', $params);
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        
+        $this->assertCount(0, $blockRepository->retrieveContents(null, null, null, 3));
+    }
+    
+    public function testFullFinalization()
+    {
+        $fileStructure = $this->client->getContainer()->getParameter('alpha_lemon_cms.theme_structure_file');
+        $blockRepository = new AlBlockRepositoryPropel();
+        $this->assertNotCount(0, $blockRepository->retrieveContents(null, null, null, 2));
+        $this->assertFileExists($fileStructure);
+        
+        $params = array('action' => 'full');
+        $crawler = $this->client->request('POST', 'backend/en/al_finalizeTheme', $params);
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        
+        $this->assertCount(0, $blockRepository->retrieveContents(null, null, null, 2));
+        $this->assertFileNotExists($fileStructure);
+    }
+    
+    public function testStartFromScratch()
+    {
+        $params = array('themeName' => 'SunshineThemeBundle');
+        $crawler = $this->client->request('POST', 'backend/en/startFromTheme', $params);
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());        
+        $this->assertCount(1, $crawler->filter('html:contains("The site has been bootstrapped with the new theme. This page is reloading")'));        
+    }
 }
