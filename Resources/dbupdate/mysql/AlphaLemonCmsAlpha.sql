8c79d9d85cec493e32070a52c32e5150e9c722df
alphalemon <user@server.com>
2013-07-01 18:55:13 +0200

    removed old update commands and added instructions about upgrading from a previous version than AlphaLemon CMS 1.1.x RC

--- Resources/dbupdate/mysql/AlphaLemonCmsAlpha.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `al_block` CHANGE `html_content` `content` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
-ALTER TABLE `al_block` CHANGE `class_name` `type` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Text';
-ALTER TABLE `al_language` CHANGE `language` `language_name` VARCHAR( 5 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
-ALTER TABLE `al_language` CHANGE `main_language` `main_language` INT( 1 ) NOT NULL DEFAULT '0';
