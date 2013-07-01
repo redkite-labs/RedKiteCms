cbad586b7f21956b6767f058c7ce9f88de277e62
alphalemon <user@server.com>
2013-07-01 18:55:13 +0200

    removed old update commands and added instructions about upgrading from a previous version than AlphaLemon CMS 1.1.x RC

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Resources/dbupdate/mysql/AlphaLemonCmsBeta4.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `al_locked_resource` (
-  `resource_name` varchar(32) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`resource_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Resources/dbupdate/mysql/AlphaLemonCmsPreAlpha1.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `al_block` SET `html_content` = REPLACE(html_content, '/bundles/alphalemoncms', '') WHERE `html_content` like '%/bundles/alphalemoncms/uploads/assets%';
-UPDATE `al_block` SET `internal_javascript` = REPLACE(internal_javascript, '/bundles/alphalemoncms', '') WHERE `internal_javascript` like '%/bundles/alphalemoncms/uploads/assets%';
