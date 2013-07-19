6a544a578bae5bbb36833f6cdef21322fad85f24
alphalemon <user@server.com>
2013-07-19 20:10:07 +0200

    updated AppKernel for Symfony 2.3

--- src/RedKiteLabs/ThemeEngineBundle/Tests/Functional/app/AppKernel.php
+++ src/RedKiteLabs/ThemeEngineBundle/Tests/Functional/app/AppKernel.php
@@ -14,12 +14,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
-            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
             new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
             new AlphaLemon\Theme\BusinessWebsiteThemeBundle\BusinessWebsiteThemeBundle(),
         );
         
