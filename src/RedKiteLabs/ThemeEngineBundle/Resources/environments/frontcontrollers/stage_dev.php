975b577219744b17f6c1e05e1ef1412586bd6838
alphalemon <user@server.com>
2013-07-21 12:10:07 +0200

    enabled debug in stage_dev frontcontroller

--- src/RedKiteLabs/ThemeEngineBundle/Resources/environments/frontcontrollers/stage_dev.php
+++ src/RedKiteLabs/ThemeEngineBundle/Resources/environments/frontcontrollers/stage_dev.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
@@ -20,6 +21,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+Debug::enable();
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('stage_dev', true);
