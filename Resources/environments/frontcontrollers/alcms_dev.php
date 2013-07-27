80befa3cca6306148d42fe2e8b9bf22872acdbc5
alphalemon <user@server.com>
2013-07-27 10:52:38 +0200

    enabled debug

--- Resources/environments/frontcontrollers/alcms_dev.php
+++ Resources/environments/frontcontrollers/alcms_dev.php
@@ -21,6 +21,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+Debug::enable();
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('alcms_dev', true);
