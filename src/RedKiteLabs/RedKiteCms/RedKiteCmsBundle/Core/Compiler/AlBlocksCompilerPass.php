76598a1037607a802cd2c53057e04fbde3997d31
redkite-cms <cms@redkite-labs.com>
2013-08-15 06:52:35 +0200

    fixed copyright disclaimer

--- src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Compiler/AlBlocksCompilerPass.php
+++ src/RedKiteLabs/RedKiteCms/RedKiteCmsBundle/Core/Compiler/AlBlocksCompilerPass.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * This file is part of the AlphaLemonPageTreeBundle and it is distributed
- * under the MIT License. In addiction, to use this bundle, you must leave
+ * This file is part of the RedKite CMS Application and it is distributed
+ * under the GPL LICENSE Version 2.0. To use this application you must leave
  * intact this copyright notice.
  *
  * Copyright (c) RedKite Labs <webmaster@redkite-labs.com>
@@ -9,9 +9,10 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * For extra documentation and help please visit http://alphalemon.com
+ * For extra documentation and help please visit http://www.redkite-labs.com
  *
- * @license    MIT License
+ * @license    GPL LICENSE Version 2.0
+ *
  */
 
 namespace RedKiteLabs\RedKiteCmsBundle\Core\Compiler;
