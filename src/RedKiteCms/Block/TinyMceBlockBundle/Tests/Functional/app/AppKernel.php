45443ed906a64571e67623538a387e3402d57975
RedKite Labs <info@redkite-labs.com>
2013-08-23 13:17:33 +0200

    block is now testable

---
+++ src/RedKiteCms/Block/TinyMceBlockBundle/Tests/Functional/app/AppKernel.php
@@ -0,0 +1,81 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
+            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
+            new Propel\PropelBundle\PropelBundle(),
+            new RedKiteLabs\BootstrapBundle\RedKiteLabsBootstrapBundle(),
+        );
+        
+        $bootstrapper = new \RedKiteLabs\BootstrapBundle\Core\Autoloader\BundlesAutoloader(__DIR__, $this->getEnvironment(), $bundles);
+        $bundles = $bootstrapper->setVendorDir(__DIR__ . '/../../../vendor')
+                                ->getBundles();
+        
+        $bundles[] = new RedKiteCms\Block\TinyMceBlockBundle\TinyMceBlockBundle();
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $configFolder = __DIR__ . '/config/bundles/config/' . $this->getEnvironment();
+        $finder = new \Symfony\Component\Finder\Finder();
+        $configFiles = $finder->depth(0)->name('*.yml')->in($configFolder);
+        foreach ($configFiles as $config) {
+            $loader->load((string) $config);
+        };
+        
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+
+    /**
+     * Credits for this awesome time-saver method to Kris Wallsmith
+     *
+     * http://kriswallsmith.net/post/27979797907
+     */
+    protected function initializeContainer()
+    {
+        static $first = true;
+
+        if (strpos($this->getEnvironment(), 'test') === false) {
+            parent::initializeContainer();
+
+            return;
+        }
+
+        $debug = $this->debug;
+
+        if (!$first) {
+            // disable debug mode on all but the first initialization
+            $this->debug = false;
+        }
+
+        // will not work with --process-isolation
+        $first = false;
+
+        try {
+            parent::initializeContainer();
+        } catch (\Exception $e) {
+            $this->debug = $debug;
+            throw $e;
+        }
+
+        $this->debug = $debug;
+    }
+}
--- src/RedKiteCms/Block/TinyMceBlockBundle/Tests/bootstrap.php
+++ src/RedKiteCms/Block/TinyMceBlockBundle/Tests/bootstrap.php
@@ -28,3 +28,36 @@ php composer.phar install
 EOT
     );
 }
+
+if (0 === strncasecmp(PHP_SAPI, 'cli', 3)) {
+    set_include_path(__DIR__ . '/../vendor/propel/propel1'.PATH_SEPARATOR.get_include_path());
+    set_include_path(__DIR__ . '/../vendor/phing/phing/classes'.PATH_SEPARATOR.get_include_path());
+}
+
+$redKiteCmsFolder = __DIR__ . '/../vendor/redkite-cms/redkite-cms-bundle/RedKiteLabs/RedKiteCmsBundle';
+require_once $redKiteCmsFolder . '/Tests/Tools/RedKiteCmsPropelQuickBuilder.php';
+if (class_exists('TypehintableBehavior')) {
+    
+    $config = array("datasources" => array (
+        "default" => array (
+            "adapter" => "sqlite",
+            "connection" => array
+            (
+                "dsn" => "sqlite::memory:",
+                "classname" => "DebugPDO",
+                "options" => array(),
+                "attributes" => array (),
+                "settings" => array (),
+            )
+        )
+    ));
+    \Propel::setConfiguration($config);
+    \Propel::initialize();
+    
+    $class = new \ReflectionClass('TypehintableBehavior');
+    $builder = new \RedKiteCmsPropelQuickBuilder();
+    $builder->getConfig()->setBuildProperty('behavior.typehintable.class', $class->getFileName());
+    $builder->setSchema(file_get_contents($redKiteCmsFolder . '/Resources/config/schema.xml'));
+    $builder->buildClasses();
+    $builder->buildSQL(\Propel::getConnection());
+}
