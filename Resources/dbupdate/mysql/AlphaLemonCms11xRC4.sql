c0dcff493cb6285bd174bc7449c9c3146ca0f24e
RedKite Labs <info@redkite-labs.com>
2013-09-01 07:02:34 +0200

    added a command to migrate from AlphaLemon RC4 to RedKite CMS

--- Resources/dbupdate/mysql/AlphaLemonCms11xRC4.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `al_configuration` (
-  `parameter` varchar(128) NOT NULL,
-  `value` varchar(128) NOT NULL,
-  PRIMARY KEY (`parameter`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-INSERT INTO `al_configuration` (`parameter`, `value`) VALUES ('language', 'en');
---
+++ Resources/dbupdate/mysql/FromAlphaLemonToRedKiteCms.sql
@@ -0,0 +1,1 @@
+UPDATE `al_block` SET `type` = 'LanguagesMenu' WHERE `type` like 'NavigationMenu' and `to_delete` = 0;
