b17404224ee68a6ecc3099d49f10e1d6d4bc703c
RedKite Labs <info@redkite-labs.com>
2013-09-18 12:47:36 +0200

    removed exception handler to let the ExceptionLister to handle exceptions

--- Controller/ConfigurationController.php
+++ Controller/ConfigurationController.php
@@ -23,43 +23,37 @@ class ConfigurationController extends Base\BaseController
 {
     public function changeCmsLanguageAction()
     {
-        try {
-            $request = $this->container->get('request');
-            $languageName = $request->get('languageName');  
-            
-            $factoryRepository = $this->container->get('red_kite_cms.factory_repository');
-            $configurationRepository = $factoryRepository->createRepository('Configuration');
-            $configuration = $configurationRepository->fetchParameter('language');                        
-            $result = $configurationRepository
-                ->setRepositoryObject($configuration)
-                ->save(array('Value' => $languageName))
-            ;
-            
-            $params = array();
-            $statusCode = 404;
-            switch ($result) {
-                case null:
-                    $message = 'configuration_controller_language_already_in_use';
-                    $params = array(
-                        '%language%' => $languageName,
-                    );
-                    break;
-                case false:
-                    // @codeCoverageIgnoreStart
-                    $message = 'configuration_controller_changing_language_error';
-                    break;
-                    // @codeCoverageIgnoreEnd
-                case true:
-                     $message = 'configuration_controller_cms_language_changed';
-                    $statusCode = 200;
-                    break;
-            }
+        $request = $this->container->get('request');
+        $languageName = $request->get('languageName');  
 
-            return new Response($this->translate($message, $params), $statusCode); 
-        } catch (\Exception $e) {
-            // @codeCoverageIgnoreStart
-            return $this->renderDialogMessage($e->getMessage());
-            // @codeCoverageIgnoreEnd
+        $factoryRepository = $this->container->get('red_kite_cms.factory_repository');
+        $configurationRepository = $factoryRepository->createRepository('Configuration');
+        $configuration = $configurationRepository->fetchParameter('language');                        
+        $result = $configurationRepository
+            ->setRepositoryObject($configuration)
+            ->save(array('Value' => $languageName))
+        ;
+
+        $params = array();
+        $statusCode = 404;
+        switch ($result) {
+            case null:
+                $message = 'configuration_controller_language_already_in_use';
+                $params = array(
+                    '%language%' => $languageName,
+                );
+                break;
+            case false:
+                // @codeCoverageIgnoreStart
+                $message = 'configuration_controller_changing_language_error';
+                break;
+                // @codeCoverageIgnoreEnd
+            case true:
+                $message = 'configuration_controller_cms_language_changed';
+                $statusCode = 200;
+                break;
         }
+
+        return new Response($this->translate($message, $params), $statusCode); 
     }
 }
