a47862db09d44cb8e5a77c7a765bf368a40e9e1e
RedKite Labs <info@redkite-labs.com>
2013-10-29 06:45:48 +0100

    removed silent mode

--- src/RedKiteLabs/RedKiteCms/InstallerBundle/Core/DsnBuilder/GenericDsnBuilder.php
+++ src/RedKiteLabs/RedKiteCms/InstallerBundle/Core/DsnBuilder/GenericDsnBuilder.php
@@ -61,7 +61,7 @@ class GenericDsnBuilder extends Base\BaseDsnBuilder
      */
     public function testConnection()
     {
-        if( ! @mysql_connect($this->configureBaseDsn(), $this->options["user"], $this->options["password"])) {
+        if( ! mysql_connect($this->configureBaseDsn(), $this->options["user"], $this->options["password"])) {
             throw new \RuntimeException("I cannot connect to the database using the given parameters");
         }
     }
