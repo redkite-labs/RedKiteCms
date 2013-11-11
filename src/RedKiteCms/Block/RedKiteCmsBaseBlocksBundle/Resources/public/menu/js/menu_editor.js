a73b9f53e0c81be8fb33283ef7e6c646c6c533ac
RedKite Labs <info@redkite-labs.com>
2013-11-11 13:04:01 +0100

    fixed menu target selector

--- src/RedKiteCms/Block/RedKiteCmsBaseBlocksBundle/Resources/public/menu/js/menu_editor.js
+++ src/RedKiteCms/Block/RedKiteCmsBaseBlocksBundle/Resources/public/menu/js/menu_editor.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         
         $(element)
             //.find('.al-menu-list')
-            .inlinelist('start', { 'target': '> li', 'position': position, 'filterAdders': 'menu' })
+            .inlinelist('start', { 'target': 'li', 'position': position, 'filterAdders': 'menu' })
         ;
     });
     
