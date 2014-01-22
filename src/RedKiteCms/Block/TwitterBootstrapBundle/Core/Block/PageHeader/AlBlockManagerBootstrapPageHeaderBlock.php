baad7e4e02e0a4d89e7e786a3fad0f872f1d821c
RedKite Labs <info@redkite-labs.com>
2014-01-22 13:22:10 +0100

    added page header block

---
+++ src/RedKiteCms/Block/TwitterBootstrapBundle/Core/Block/PageHeader/AlBlockManagerBootstrapPageHeaderBlock.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * This file is part of the TwitterBootstrapBundle and it is distributed
+ * under the MIT LICENSE. To use this application you must leave intact this copyright
+ * notice.
+ *
+ * Copyright (c) RedKite Labs <info@redkite-labs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.redkite-labs.com
+ *
+ * @license    MIT LICENSE
+ *
+ */
+
+namespace RedKiteCms\Block\TwitterBootstrapBundle\Core\Block\PageHeader;
+
+use RedKiteLabs\RedKiteCmsBundle\Core\Content\Block\JsonBlock\AlBlockManagerJsonBlockContainer;
+
+/**
+ * Defines the Block Manager to handle the Bootstrap Label
+ *
+ * @author RedKite Labs <info@redkite-labs.com>
+ */
+class AlBlockManagerBootstrapPageHeaderBlock extends AlBlockManagerJsonBlockContainer
+{
+    /**
+     * Defines the App-Block's default value
+     *
+     * @return array
+     */
+    public function getDefaultValue()
+    {
+        $value =
+            '
+                {
+                    "0": {
+                        "page_header_title": "Page Header",
+                        "page_header_subtitle": "An awesome component",
+                        "page_header_tag": "h1"
+                    }
+                }
+            ';
+
+        return array('Content' => $value);
+    }
+
+    /**
+     * Renders the App-Block's content view
+     *
+     * @return string|array
+     */
+    protected function renderHtml()
+    {
+        $item = $this->decodeJsonContent($this->alBlock->getContent());
+
+        return array('RenderView' => array(
+            'view' => 'TwitterBootstrapBundle:Content:PageHeader/page_header.html.twig',
+            'options' => array('data' => $item[0]),
+        ));
+    }
+
+    /**
+     * Defines the parameters passed to the App-Block's editor
+     *
+     * @return array
+     */
+    public function editorParameters()
+    {
+        $item = $this->decodeJsonContent($this->alBlock->getContent());
+        
+        $formClass = $this->container->get('bootstrap_page_header_block.form');
+        $form = $this->container->get('form.factory')->create($formClass, $item[0]);
+
+        return array(
+            "template" => "TwitterBootstrapBundle:Editor:PageHeader/page_header_editor.html.twig",
+            "title" => $this->translator->translate('page_header_editor_title', array(), 'TwitterBootstrapBundle'),
+            "form" => $form->createView(),
+        );
+    }
+}
---
+++ src/RedKiteCms/Block/TwitterBootstrapBundle/Core/Form/PageHeader/AlPageHeaderType.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is part of the TwitterBootstrapBundle and it is distributed
+ * under the MIT LICENSE. To use this application you must leave intact this copyright
+ * notice.
+ *
+ * Copyright (c) RedKite Labs <info@redkite-labs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * For extra documentation and help please visit http://www.redkite-labs.com
+ *
+ * @license    MIT LICENSE
+ *
+ */
+
+namespace RedKiteCms\Block\TwitterBootstrapBundle\Core\Form\PageHeader;
+
+use RedKiteCms\Block\TwitterBootstrapBundle\Core\Form\Base\AlBaseType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Defines the form to edit a label block
+ *
+ * @author RedKite Labs <info@redkite-labs.com>
+ */
+class AlPageHeaderType extends AlBaseType
+{
+    /**
+     * Builds the form
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('page_header_title', null, array('label' => 'page_header_title',));
+        $builder->add('page_header_subtitle', null, array('label' => 'page_header_subtitle',));
+        $builder->add('page_header_tag', 'choice', array(
+            'label' => 'page_header_tag',
+            'choices' => array(
+                'h1' => 'h1',
+                'h2' => 'h2',
+                'h3' => 'h3',
+                'h4' => 'h4',
+                'h5' => 'h5',
+                'h6' => 'h6',
+            )
+        ));
+
+        parent::buildForm($builder, $options);
+    }
+}
