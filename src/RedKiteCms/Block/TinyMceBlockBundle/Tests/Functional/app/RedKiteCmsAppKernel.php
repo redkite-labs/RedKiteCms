19f28cd7c76403361478b46c0515b580338c056b
RedKite Labs <info@redkite-labs.com>
2014-03-14 05:57:16 +0100

    added RedKiteCmsAppKernel

--- src/RedKiteCms/Block/TinyMceBlockBundle/Tests/Functional/app/RedKiteCmsAppKernel.php
+++ src/RedKiteCms/Block/TinyMceBlockBundle/Tests/Functional/app/RedKiteCmsAppKernel.php
@@ -3,7 +3,7 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
+class RedKiteCmsAppKernel extends Kernel
 {
     public function registerBundles()
     {
@@ -19,15 +19,15 @@ class AppKernel extends Kernel
             new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
             new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
             new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
-            new Propel\PropelBundle\PropelBundle(),
             new RedKiteLabs\RedKiteCms\BootstrapBundle\RedKiteLabsBootstrapBundle(),
+            new Propel\PropelBundle\PropelBundle(),
         );
         
         $bootstrapper = new \RedKiteLabs\RedKiteCms\BootstrapBundle\Core\Autoloader\BundlesAutoloader(__DIR__, $this->getEnvironment(), $bundles);
         $bundles = $bootstrapper->setVendorDir(__DIR__ . '/../../../vendor')
                                 ->getBundles();
         
-        $bundles[] = new RedKiteCms\Block\TinyMceBlockBundle\TinyMceBlockBundle();
+        $bundles[] = new RedKiteLabs\RedKiteCms\RedKiteCmsBundle\RedKiteCmsBundle();
 
         return $bundles;
     }
